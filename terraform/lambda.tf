0b87d8f9282280bba948dc4c342d40c29bfd66ea
Rocky Gray <rocky.grayjr@gmail.com>
2018-08-12 18:27:04 -0400

    give the gateway permission to access the lambda function

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -34,3 +34,14 @@ resource "aws_lambda_function" "ddns-service" {
   role = "${aws_iam_role.lambda_exec.arn}"
 }
 
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.ddns-service.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_deployment.ddns-service.execution_arn}/*/*"
+}
+
