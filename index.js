b0ff0c5f0772d0f1e86aaa44e4b26af70b5909ba
Rocky Gray <rocky.grayjr@gmail.com>
2018-08-12 23:09:02 -0400

    initial impl for public-ip

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
+// const api = require('./src/lib/api');
+const Router = require('./src/lib/simple-router');
+
+const router = new Router({
+  middleware: [
+    (event) => {
+      console.log(event);
+      return event;
+    }
+  ],
+  routes: [
+    'GET', '/public-ip', (event) => {
+      const clientIp = event.headers['X-Forwarded-For'] || 'unknown';
+      return {
+        statusCode: 200,
+        body: {
+          publicIp: clientIp
+        }
+      }
+    }
+  ],
+  defaultRoute: (event) => ({
+    statusCode: 404,
+    body: {
+      message: 'Resource not found',
+      resource: event.path
+    }
+  })
+});
+
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html; charset=utf-8',
-    },
-    body: JSON.stringify({ event, context }, null, 2),
-  };
+  event._context = context;
+  try {
+    return await router.route(event);
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: e.message
+    };
+  }
 };
 
---
+++ src/lib/simple-router.js
@@ -0,0 +1,45 @@
+// https://raw.githubusercontent.com/trek10inc/lambda-router/1925775867086dada0fd70cf17cc951f7f909730/index.js
+const bbPromise = require('bluebird');
+const pathToRegexp = require('path-to-regexp');
+
+class Router {
+  constructor (options) {
+    this.middleware = options.middleware || [];
+    this.routes = options.routes || [];
+    this.defaultRoute = options.defaultRoute
+  }
+
+  route (event) {
+    // execute middleware
+    return bbPromise.reduce(this.middleware, (event, middleware) => middleware(event), event)
+      .then(middleWaredEvent => {
+        // find route
+        let route = this.routes.find(route => this.checkRoute(route, event)) || [null, null, this.defaultRoute];
+        // execute route
+        return bbPromise.reduce((Array.isArray(route[2]) ? route[2] : [route[2]]), (event, middleware) => middleware(event), middleWaredEvent);
+      })
+  }
+
+  checkRoute (route, event) {
+    // match method and path
+    let methodMatches = route[0].toUpperCase() === 'ANY'
+      || ((event.httpMethod || '').toUpperCase() === route[0].toUpperCase());
+
+    if (methodMatches) {
+      let keys = [];
+      let re = pathToRegexp(route[1], keys);
+      let results = re.exec(event.path);
+      if (results) {
+        // add parsed path results to event.pathParameters to make it easy to interchange with apigateway routing
+        event.pathParameters = event.pathParameters || {};
+        keys.forEach((key, index) => {
+          event.pathParameters[key.name] = results[index + 1];
+        });
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+module.exports = Router;
