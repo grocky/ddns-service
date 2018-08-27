3fab44dbaf5bf79f00335b52378efe30dd6618b8
Rocky Gray <rocky.grayjr@gmail.com>
2018-08-26 21:06:19 -0400

    initial impl for go service

--- Makefile
+++ Makefile
@@ -6,7 +6,19 @@ APP_ARCHIVE=$(PROJECT_NAME)-$(APP_VERSION).zip
 
 BUILD_DIR=bin
 
+LOCAL_OS := $(shell uname | tr '[:upper:]' '[:lower:]')
+
 ##### Targets ######
+
+.PHONY: build-local ${BUILD_DIR}/${PROJECT_NAME}_${LOCAL_OS}
+build-local: ${BUILD_DIR}/${PROJECT_NAME}_${LOCAL_OS}
+${BUILD_DIR}/${PROJECT_NAME}_${LOCAL_OS}:
+	env GOOS=${LOCAL_OS} GOARCH=amd64 go build -o ${BUILD_DIR}/${PROJECT_NAME}_${LOCAL_OS} main.go
+
+.PHONY: build ${BUILD_DIR}/${PROJECT_NAME}_linux
+build: ${BUILD_DIR}/${PROJECT_NAME}_linux
+${BUILD_DIR}/${PROJECT_NAME}_linux:
+	env GOOS=linux GOARCH=amd64 go build -o ${BUILD_DIR}/${PROJECT_NAME}_linux main.go
 
 .PHONY: package
 package: _ensure-package _archive-source
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+  "encoding/json"
+  "fmt"
+  "github.com/aws/aws-lambda-go/events"
+  "github.com/aws/aws-lambda-go/lambda"
+  "log"
+  "net/http"
+  "os"
+)
+
+var errorLogger = log.New(os.Stderr, "ERROR: ", log.Llongfile)
+
+type ClientIpResponse struct {
+  PublicIp string `json:"publicIp"`
+}
+
+type ErrorResponse struct {
+  Description string `json:"description"`
+}
+
+func handleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+  clientIp := request.Headers["X-Forwarded-For"]
+
+  if clientIp == "" {
+    return clientError(http.StatusBadRequest, "Client IP not found");
+  }
+
+  js, err := json.Marshal(ClientIpResponse{clientIp})
+  if err != nil {
+   return serverError(err)
+  }
+
+  return events.APIGatewayProxyResponse{
+    StatusCode: 200,
+    Body: string(js),
+  }, nil
+}
+
+func serverError(err error) (events.APIGatewayProxyResponse, error) {
+  errorLogger.Println(err.Error())
+
+  return events.APIGatewayProxyResponse{
+    StatusCode: http.StatusInternalServerError,
+    Body:       buildErrorResponse(err.Error()),
+  }, nil
+}
+
+// Similarly add a helper for send responses relating to client errors.
+func clientError(status int, description string) (events.APIGatewayProxyResponse, error) {
+  return events.APIGatewayProxyResponse{
+    StatusCode: status,
+    Body: buildErrorResponse(description),
+  }, nil
+}
+
+func buildErrorResponse(description string) string {
+  response := ErrorResponse{Description: description}
+  js, err := json.Marshal(response)
+
+  if (err != nil) {
+    errorLogger.Println(response)
+    return "Unable to marshl response."
+  }
+
+  return string(js)
+}
+
+func main() {
+  fmt.Print(os.Getenv("_LAMBDA_SERVER_PORT"))
+  lambda.Start(handleRequest)
+}
