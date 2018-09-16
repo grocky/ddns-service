a1013b63f98df82ebc8988533d6e0adec056df7c
Rocky Gray <rocky.grayjr@gmail.com>
2018-09-16 18:42:27 -0400

    Extract assume role policy json and add lambda role policy for logging

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,24 +4,13 @@ provider "aws" {
 
 variable "app_version" { }
 
+data "template_file" "ddns-service-lambda-assume-role-policy" {
+  template = "${file("${path.module}/lambda-assume-role-policy.json")}"
+}
+
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = "${data.template_file.ddns-service-lambda-assume-role-policy.rendered}"
 }
 
 resource "aws_lambda_function" "ddns-service" {
@@ -34,6 +23,22 @@ resource "aws_lambda_function" "ddns-service" {
   runtime = "go1.x"
 
   role = "${aws_iam_role.lambda_exec.arn}"
+}
+
+data "template_file" "ddns-service-lambda-role-policy" {
+  template = "${file("${path.module}/lambda-role-policy.json")}"
+}
+
+resource "aws_iam_policy" "ddns-service-policy" {
+  name   = "ddns-service-policy"
+  path   = "/"
+  policy = "${data.template_file.ddns-service-lambda-role-policy.rendered}"
+}
+
+resource "aws_iam_policy_attachment" "ddns-service_policy_attachment" {
+  name       = "ddns-service-policy-attachment"
+  roles      = ["${aws_iam_role.lambda_exec.name}"]
+  policy_arn = "${aws_iam_policy.ddns-service-policy.arn}"
 }
 
 resource "aws_lambda_permission" "apigw" {
